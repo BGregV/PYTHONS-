b5f97f9bd4d05e98d436d42717eba988d3019bc3
BGregV <greguar@ninodom.ru>
2022-12-18 21:12:29 +0300

    add info lec2

--- lec.py
+++ lec.py
@@ -1,3 +1,5 @@
+# Лекция 1
+
 # value = None
 # a = 123
 # b = 1.23
@@ -52,8 +54,8 @@
 # print(func<T>x)
 
 # f = [1,2,3,4]
-# # print(f)
-# # print(not 2 in f)
+# print(f)
+# print(not 1 in f)
 
 
 # is_odd = f[0] % 2
@@ -160,3 +162,161 @@
 # arg = 2.3
 # print(f(arg))
 # print(type(f(arg)))
+
+# Лекция 2
+
+
+# Хренение данных
+# Передача данных в клиент-серверных проектах
+# Хронение конфигов
+# Логирование действий
+
+# Связать файловую переменную с файлом, определив модификатор работ:
+# a - открытие для добавления данных
+# r - открытие для чтения данных
+# w - отклытие для записи данных
+# w+, r+
+
+
+# colors = ['red', 'geen', 'blue']
+# data = open('file.txt', 'a')
+# # data.writelines(colors) # разделителя не будет
+# data.write('\nLINE 12\n')
+# data.write('LINE 13\n')
+# data.close()
+
+
+# второе решение для создания и записи в файле
+# with open('file.txt', 'w') as data:
+#     data.write('LINE 12\n')
+#     data.write('LINE 13\n')
+
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# импорт данных
+# import sem.py as s
+# print(s.f(1))
+
+# def new_string(symbol, count = 3):
+#     return symbol * count
+# print(new_string('!', 5))
+# print(new_string('!'))
+# print(new_string('4'))
+
+# def concatenatio(*params):
+#     res: str = ""
+#     for item in params:
+#         res += item
+#     return res
+
+# print(concatenatio('a', 's', 'd', 'w'))
+# print(concatenatio('a', '1'))
+
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+
+
+# list = []
+# for e in range(1, 10):
+#     list.append(fib(e))
+# print(list)
+
+
+# Кортежи это неизменяемый список
+# Кортеж может быть и из одного числа, то необходимо после числа ставить ","
+
+# a = (3, 4)
+# print(a[0])
+
+# Словари - неупорядоченные коллекции произвольных объектов с доступом по ключу
+
+# distionary = {}
+# distionary = \
+#     {
+#         'up': '↑',
+#         'left': '←',
+#         'down': '↓',
+#         'right': '→'
+#     }
+# print(distionary)
+# print(distionary['left'])
+
+# for k in distionary.keys():
+#     print(k)
+# for k in distionary.values():
+#     print(k)
+# for v in distionary:
+#     print(v)
+
+
+
+# Множества
+
+# colors = {'red', 'geen', 'blue'}
+# print(colors)
+# colors.add('red')
+# print(colors)
+# colors.add('grey')
+# print(colors)
+# colors.remove('red')
+# print(colors)
+# colors.discard('red')
+# print(colors)
+# colors.clear()
+# print(colors)
+
+
+# a = {1, 2, 3, 4, 5}
+# b = {2, 8, 13, 5, 21}
+# c = a.copy()
+# u = a.union(b)
+# i = a.intersection(b)
+# dl = a.difference(b)
+# dr = b.difference(a)
+
+# q = a \
+#     .union(b) \
+#     .difference(a.difference(b))
+# print(q)
+
+
+# Списки
+# list = [1, 2, 3, 4, 5]
+# list2 = list
+
+# for e in list:
+#     print(e)
+# print()
+
+# for e in list2:
+#     print(e)
+
+
+# list[0] = 123
+# for e in list:
+#     print(e)
+# print()
+
+# for e in list2:
+#     print(e)
+
+# list = [1, 2, 3, 4, 5]
+# print(len(list))
+# print(list.pop())
+# print(list.pop())
+# print(list.pop())
+# print(list.pop())
+
+# print(list.pop(2))
+# print(list)
+
+# print(list.insert(2, 11))
+# print(list)
